a2fd41c3eb4a2304ab5b1e2b2f2b6d2385877f4a
Zack <zackmcginnis@hotmail.com>
2015-10-07 15:28:30 -0700

    Initial commit for class

---
+++ cs441/mcginnis_15puz.lisp
@@ -0,0 +1,9 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+Zack McGinnis
+903343191
+Laboratory 1, CS441, 15-puzzle
+Date: 10/09/2015
+Instructions: In order to run this code, load the file and
+Type (solve) at the clisp prompt. Your “solve’ function
+Should prompt for the name of the input file
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
